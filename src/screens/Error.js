222a8fa88b51a3cfe256b23c977d21e4b887042c
wolz-CODElife <joeladewole3@gmail.com>
2021-08-16 04:05:33 +0100

    Changed a tags to link

--- src/screens/Error.js
+++ src/screens/Error.js
@@ -6,7 +6,7 @@ import './css/Error.css'
 const Error = () => {
     return (
         <div className="error">
-            <Link className="error_back_button"><HomeRoundedIcon /> Back to Home</Link>
+            <Link to="/" className="error_back_button"><HomeRoundedIcon /> Back to Home</Link>
             <img src="https://i.postimg.cc/pXpWB9dr/404-Error-Page-not-Found-with-people-connecting-a-plug-amico.png" alt="Web illustrations by Storyset" className="" />
             <a href="https://storyset.com/web">Web illustrations by Storyset</a>
         </div>
--- src/screens/components/MenuOption.js
+++ src/screens/components/MenuOption.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './MenuOption.css'
 
 const MenuOption = ({Icon, title, more, link, logout}) => {
     return (
-        <p className={more? 'menu_option more': 'menu_option'} onClick={() => logout&& logout()}  style={{ cursor: 'pointer' }}>
-            <a href={link} title={title} className="menu">
+        <p className={more ? 'menu_option more' : 'menu_option'} onClick={() => logout && logout()} style={{ cursor: 'pointer' }}>
+            <Link to={link} title={title} className="menu">
                 <Icon fontSize="large" className="menu_icon" />
                 <span className="menu_title">{ title }</span>
-            </a>
+            </Link>
         </p>
     )
 }
